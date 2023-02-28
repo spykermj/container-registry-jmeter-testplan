728e4e97e7b0df1c5121abc818722d8271d4046f
Jeremy Spykerman <jeremy.spykerman@sky.uk>
2023-02-28 13:28:34 +0000

    Add a new property
    When using buildah, it is nice to set the authentication without needing
    interaction. Passing in a new property to accomodate setting that
    environment variable facilitates working non-interactively with buildah.

--- run_test.sh
+++ run_test.sh
@@ -2,4 +2,4 @@
 
 mkdir -p testlogs results
 
-./jmeter/apache-jmeter-5.5/bin/jmeter -t container-registry-performance-test.jmx -p perftest.properties -n
+./jmeter/apache-jmeter-5.5/bin/jmeter -t container-registry-performance-test.jmx -p docker.properties -n
